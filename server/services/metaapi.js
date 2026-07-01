f1bcee83f2075f27e984b370c3750e664acba2b0
maliczen1-oss <maliczen1@gmail.com>
2026-07-01 17:59:30 +0200

    Update metaapi.js

--- server/services/metaapi.js
+++ server/services/metaapi.js
@@ -1,1 +1,51 @@
+const MetaApi = require("metaapi.cloud-sdk").default;
 
+let api = null;
+let account = null;
+let connection = null;
+
+async function initialize(token, accountId) {
+
+    api = new MetaApi(token);
+
+    account =
+        await api.metatraderAccountApi.getAccount(
+            accountId
+        );
+
+    if (account.state !== "DEPLOYED") {
+        await account.deploy();
+    }
+
+    await account.waitConnected();
+
+    connection =
+        account.getRPCConnection();
+
+    await connection.connect();
+
+    await connection.waitSynchronized();
+
+    console.log(
+        "MetaApi synchronized successfully."
+    );
+
+}
+
+function getConnection() {
+    return connection;
+}
+
+function getAccount() {
+    return account;
+}
+
+module.exports = {
+
+    initialize,
+
+    getConnection,
+
+    getAccount
+
+};
