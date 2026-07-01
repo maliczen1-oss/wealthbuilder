354143bd1f2a9425c016dc1dcc077feea36377bb
maliczen1-oss <maliczen1@gmail.com>
2026-07-02 01:02:22 +0200

    Create constants.js

---
+++ server/config/constants.js
@@ -0,0 +1,34 @@
+/*
+==========================================================
+WealthBuilder OS
+System Constants
+==========================================================
+*/
+
+module.exports = {
+
+    VERSION: "0.95.0",
+
+    CODENAME: "MISSION CONTROL",
+
+    COMPANY: "WealthBuilder OS",
+
+    AI_NAME: "Jarvis",
+
+    DEFAULT_REFRESH_INTERVAL: 30000,
+
+    MAX_NOTIFICATIONS: 200,
+
+    MAX_LOG_ENTRIES: 1000,
+
+    GUARDIAN: {
+
+        MIN_MARGIN_LEVEL: 150,
+
+        MAX_OPEN_POSITIONS: 4,
+
+        DEFAULT_RISK: 1
+
+    }
+
+};
