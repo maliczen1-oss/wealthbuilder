15edd7b3d9b0e6e3868c1873240303df5ef70418
maliczen1-oss <maliczen1@gmail.com>
2026-07-01 18:25:56 +0200

    Update brokerProfiles.js

--- server/config/brokerProfiles.js
+++ server/config/brokerProfiles.js
@@ -1,1 +1,114 @@
+const brokerProfiles = {
 
+  default: {
+
+    name: "Generic MT5",
+
+    accountTypes: [
+      "STANDARD",
+      "MICRO",
+      "CENT"
+    ],
+
+    symbolAliases: {
+
+      EURUSD: [
+        "EURUSD",
+        "EURUSD.a",
+        "EURUSDm",
+        "mEURUSD",
+        "EURUSD.pro"
+      ],
+
+      GBPUSD: [
+        "GBPUSD",
+        "GBPUSD.a",
+        "GBPUSDm",
+        "mGBPUSD"
+      ],
+
+      USDJPY: [
+        "USDJPY",
+        "USDJPYm",
+        "mUSDJPY"
+      ],
+
+      XAUUSD: [
+        "XAUUSD",
+        "XAUUSDm",
+        "GOLD"
+      ],
+
+      NAS100: [
+        "NAS100",
+        "mNAS100",
+        "USTEC",
+        "US100",
+        "NAS100.cash"
+      ]
+
+    }
+
+  },
+
+  vaultMarkets: {
+
+    name: "Vault Markets",
+
+    accountTypes: [
+
+      "STANDARD",
+
+      "MICRO",
+
+      "CENT"
+
+    ],
+
+    baseCurrency: "ZAR",
+
+    preferredSymbols: {
+
+      EURUSD: "mEURUSD",
+
+      NAS100: "mNAS100",
+
+      XAUUSD: "XAUUSD"
+
+    },
+
+    risk: {
+
+      defaultRiskPercent: 1,
+
+      maxOpenPositions: 4,
+
+      maxDailyLossPercent: 5
+
+    }
+
+  }
+
+};
+
+function getBrokerProfile(brokerName = "") {
+
+  const broker = brokerName.toLowerCase();
+
+  if (broker.includes("vault")) {
+
+    return brokerProfiles.vaultMarkets;
+
+  }
+
+  return brokerProfiles.default;
+
+}
+
+module.exports = {
+
+  brokerProfiles,
+
+  getBrokerProfile
+
+};
