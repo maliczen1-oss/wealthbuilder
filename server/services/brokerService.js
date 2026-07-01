4afc3ee30c57eaf1eba9f393ac35c2ee42a2b67c
maliczen1-oss <maliczen1@gmail.com>
2026-07-01 18:30:20 +0200

    Update brokerService.js

--- server/services/brokerService.js
+++ server/services/brokerService.js
@@ -1,1 +1,83 @@
+const accountService = require("./accountService");
+const symbolService = require("./symbolService");
 
+const {
+  getBrokerProfile
+} = require("../config/brokerProfiles");
+
+async function detectBroker() {
+
+  const account =
+    await accountService.getAccount();
+
+  const profile =
+    getBrokerProfile(
+      account.broker || ""
+    );
+
+  return {
+
+    broker: account.broker,
+
+    server: account.server,
+
+    currency: account.currency,
+
+    leverage: account.leverage,
+
+    profile
+
+  };
+
+}
+
+async function detectSymbols() {
+
+  return {
+
+    EURUSD:
+      await symbolService.getEURUSD(),
+
+    GBPUSD:
+      await symbolService.getGBPUSD(),
+
+    USDJPY:
+      await symbolService.getUSDJPY(),
+
+    XAUUSD:
+      await symbolService.getXAUUSD(),
+
+    NAS100:
+      await symbolService.getNAS100()
+
+  };
+
+}
+
+async function getEnvironment() {
+
+  const broker =
+    await detectBroker();
+
+  const symbols =
+    await detectSymbols();
+
+  return {
+
+    broker,
+
+    symbols
+
+  };
+
+}
+
+module.exports = {
+
+  detectBroker,
+
+  detectSymbols,
+
+  getEnvironment
+
+};
